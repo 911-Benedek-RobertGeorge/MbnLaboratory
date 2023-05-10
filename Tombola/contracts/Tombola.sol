c319b07fe659286f7766231f381b13cb13ea17d8
Benedek Robert George <72063093+911-Benedek-RobertGeorge@users.noreply.github.com>
2023-05-10 17:15:31 +0300

    all functionalities

--- Tombola/contracts/Tombola.sol
+++ Tombola/contracts/Tombola.sol
@@ -5,19 +5,20 @@ contract Tombola {          /// TODO verify it on etherscan
      
     address payable public owner;
 
-    // Dan 
-    // isAdmin modifier 
+     
+    modifier isAdmin(){
+        require(  msg.sender  == owner, "Only owner is allowed to do this!");
+        _;
+    }
 
     struct TombolaStruct {
         string nume;
         uint startTime;  //unix  Timestamp
         uint endTime;
         bool isRunning; 
-        // mapping(address => bool) tickets;
         address[] tickets; //  made an array so it will be reseted when delete the struct, the mappings cant be deleted
-         // uint nrOfParticipants;  // delete bcs tickets.length
         uint price;
-        // uint prizePool;  // we can delete this bcs is equal to nrOfParticipants*price
+         
     }
 
     TombolaStruct public tombola; 
@@ -29,23 +30,24 @@ contract Tombola {          /// TODO verify it on etherscan
         return tombola.tickets.length * tombola.price;
     }
 
-    constructor() payable {  // DAN 
-        // sa ti dai seama cum sa selectezi owner ul 
-      
+    constructor() payable {  
+         
+        owner = payable(msg.sender);
     }
     
-    // DAN 
-    function startTombola( parametri startTime , endDate, price, prize) public isAdmin { 
-        //require(isAdmin,"You have to be the owner of this contract");
-        // check uri 
-        // tombola.isRunning == false 
-        tombola.startDate = startTime;  
-
+   
+    function startTombola(string memory _name, uint _startTime , uint _endTime, uint  _ticketPrice) public isAdmin { 
+        require(tombola.isRunning == false,"THere already is a tombola runnnig");
+        require(block.timestamp < _endTime,"Verify again the time slots.");
+        tombola.startTime = _startTime;  
+        tombola.endTime = _endTime;  
+        tombola.price = _ticketPrice;
+        tombola.isRunning = true;
     }
 
-    //beni
+    
     function buyTicket() internal {
-         //cand apasa pe buton o sa trebuiasca sa i dea allowance la contract 
+         //cand apasa pe buton o sa trebuiasca sa i dea allowance la adresa  contract 
         require(tombola.isRunning == true, "Tombola has to be running");
         require(msg.value == tombola.price, "The ticket is more expensive");
         for(uint i = 0; i < tombola.tickets.length; ++i) {
@@ -60,29 +62,35 @@ contract Tombola {          /// TODO verify it on etherscan
     receive() external payable {
         buyTicket();
     }
-    //Beni 
+
+
+    event FinalizareTombola(address winner1, address winner2);
+
     function finalizareTombola() public isAdmin returns(address, address){
         //maybe add an oracle for random number 
         require(block.timestamp > tombola.endTime , "The Tombola is still running.");
-        require(tombola.nrOfParticipants > 2, "Not enough participants.");
+        require(tombola.tickets.length > 2, "Not enough participants.");
         uint nrOfParticipants = tombola.tickets.length;
-        uint prizePool = tombola.price * tombola.nrOfParticipants;
-        uint randNr = uint (keccak256(abi.encodePacked (msg.sender, block.timestamp,  nrOfParticipants))) % nrOfParticipants;
-        
-        address winner1 = tombola.tickets[randNr];
-        randNr = uint (keccak256(abi.encodePacked (msg.sender, block.timestamp, randNr))) % nrOfParticipants;
-        uint prize1 = (prizePool / 100 ) * 70 ; //overflow safe
+        uint prizePool = tombola.price * nrOfParticipants;
 
+        uint randNr = uint (keccak256(abi.encodePacked (msg.sender, block.timestamp,  nrOfParticipants))) % nrOfParticipants;
+        address winner1 = tombola.tickets[randNr];
+        uint prize1 = (prizePool / 100 ) * 70 ; //overflow safe
         payable(winner1).transfer(prize1);
 
-        address winner2 = tombola.tickets[randNr];
-        uint prize2 = (prizePool / 100 ) * 25 ;  
+        uint randNr2 = uint (keccak256(abi.encodePacked (msg.sender, block.timestamp, randNr))) % nrOfParticipants;
 
+        while(randNr == randNr2){
+            randNr2 = uint (keccak256(abi.encodePacked (msg.sender, block.timestamp, randNr2))) % nrOfParticipants;
+        }
+        address winner2 = tombola.tickets[randNr2];
+        uint prize2 = (prizePool / 100 ) * 25 ;  
         payable(winner2).transfer(prize2);
 
         delete tombola;  // reset the tombola to be ready for the next one
-
+        
         // maybe emit an event
+        emit FinalizareTombola(winner1, winner2);
         return (winner1,winner2);
 
     }
