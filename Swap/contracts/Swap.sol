a2cfe0206472dd6c12e149ef07cc33c6866cb138
Benedek Robert George <72063093+911-Benedek-RobertGeorge@users.noreply.github.com>
2023-05-14 19:14:52 +0300

    working code

--- Swap/contracts/Swap.sol
+++ Swap/contracts/Swap.sol
@@ -1,40 +1,42 @@
-import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+/ SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
 
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 
-contract Swap {
-  address private constant UNISWAP_V2_ROUTER =
-    0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+contract Exchange {
+    address private constant UNISWAP_V2_ROUTER =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
-  address public owner ; 
-  IUniswapV2Router02 private router = IUniswapV2Router02(UNISWAP_V2_ROUTER);
+    address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
-  constructor(address theInterface) {
-    owner = msg.sender;
-  }
-    function swapETHForTokenSupportingFeeOnTransferTokens(
-    address token,
-    uint amount,
-    uint deadline
-  ) external payable  returns (uint amountOut) {
-    address[] memory path = new address[](2);
-    path[0] = router.WETH();
-    path[1] = token; //usdt
+    IUniswapV2Router02 private router = IUniswapV2Router02(UNISWAP_V2_ROUTER);
+   //  IERC20 private weth = IERC20(WETH);
+   //  IERC20 private dai = IERC20(DAI);
 
-    uint[] memory amounts = router.swapExactETHForTokensSupportingFeeOnTransferTokens{value: msg.value}(
-     
-      0,
-      path,
-      msg.sender,
-      deadline
-    );
-    
-    uint ownerPart = amounts[1] * 1 / 100 ;
-    IERC20(token).transfer(owner, ownerPart); // transfer commission fee to owner
-    IERC20(token).transfer(msg.sender, amounts[1] - ownerPart); // transfer remaining tokens to user
-    return amounts[1];
 
-  }
+
+    // Swap WETH to USDC
+    function swapSingleHopExactAmountOut(
+    ) external payable returns (uint amountOut) {
+      //   weth.transferFrom(msg.sender, address(this), amountInMax);
+      //   weth.approve(address(router), amountInMax);
+
+        address[] memory path;
+        path = new address[](2);
+        path[0] = router.WETH();
+        path[1] = USDC;
+        address sender = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
+      //   _approve(sender, address(uniswapV2Router02), amount);
+        uint[] memory amounts = router.swapExactETHForTokens{value: msg.value}(
+            0,
+            path,
+            sender,
+            1686498662
+        ); 
+      //   IERC20(USDC).transfer(sender, amounts[1]);
+        return amounts[1];
+    }
 }
--- Swap/test/SwapTest.js
+++ Swap/test/SwapTest.js
@@ -3,12 +3,12 @@ const { expect } = require("chai");
 describe("EXCHANGE", function () {
 	it(".", async function () {
 		const [owner] = await ethers.getSigners();
-		const USDT = "0xdac17f958d2ee523a2206206994597c13d831ec7";
-		const Token = await ethers.getContractFactory("Swap");
+
+		const Token = await ethers.getContractFactory("Exchange");
 
 		const hardhatToken = await Token.deploy();
 
-		const ownerBalance = await hardhatToken.swapETHForTokenSupportingFeeOnTransferTokens(USDT, 0, 123412341234, {
+		const ownerBalance = await hardhatToken.swapSingleHopExactAmountOut({
 			value: "10000000000000000",
 		});
 		console.log(ownerBalance);
