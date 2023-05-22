7fb782c4ee55fb67f05d95639afe2e5c99411820
Benedek Robert George <72063093+911-Benedek-RobertGeorge@users.noreply.github.com>
2023-05-22 23:17:40 +0300

    fix bugs

--- Staking/contracts/Staking.sol
+++ Staking/contracts/Staking.sol
@@ -10,12 +10,14 @@ contract Staking {
 
     IERC20 public xCoinContract ;
 
-    mapping(address => uint) staked;
-    mapping(address => uint) rewards;
+    mapping(address => uint) public staked;
+    mapping(address => uint) public rewards;
     address[] public users; 
     uint public currentRewards;
     uint public totalStaked;
 
+
+    
     constructor(address payable tokenAddress){
         xCoinContract = XCoin(tokenAddress);
     }
@@ -24,20 +26,20 @@ contract Staking {
 
     function stake(uint _amount) public {
         // approve (give allowance ) call from the Xcoin contract 
-        require(xCoinContract.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
-        require(xCoinContract.balanceOf(msg.sender) >= _amount, "Not enough balance!");
+    require(xCoinContract.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
 
         if(currentRewards > 0){
             shareRewards();
         }
+
         if(verifyUser(msg.sender)){ 
             users.push(msg.sender);
         }   
-        staked[msg.sender] = _amount;
+        staked[msg.sender] += _amount;
         totalStaked += _amount;
     }
 
-    function verifyUser(address origin) internal returns(bool) {
+    function verifyUser(address origin) internal view returns(bool) {
         for(uint i = 0; i < users.length; ++i){
             if(users[i] == origin) return false;
         }
@@ -53,16 +55,18 @@ contract Staking {
         for(uint i = 0; i < users.length; ++i){
             user = users[i];
             amountStaked = staked[user];
-            personalReward =  currentRewards * (amountStaked / totalStaked); // check this formula
+            personalReward =  currentRewards * amountStaked  / totalStaked ; // check this formula
             rewards[user] += personalReward;
         }
+        currentRewards = 0;
     }
 
     function withdrawStake(uint _amount) public{
-        require(staked[msg.sender] > _amount, "You have less staked.");
+        require(staked[msg.sender] >= _amount, "You have less staked.");
           
-        xCoinContract.transfer(msg.sender,staked[msg.sender]);
+        xCoinContract.transfer(msg.sender,_amount);
         staked[msg.sender] -= _amount;
+        totalStaked -= _amount;
     }
      
     function claim() public{
@@ -73,7 +77,7 @@ contract Staking {
     }
  
     function getLoan() public { 
-
+        currentRewards = 1000;
     }
     
  
