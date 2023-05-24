8b617f65a52da5409afeba8c3a09885f6843f0ef
Benedek Robert George <72063093+911-Benedek-RobertGeorge@users.noreply.github.com>
2023-05-25 01:11:25 +0300

    contract function calls working

--- exchange-frontend/src/App.js
+++ exchange-frontend/src/App.js
@@ -1,12 +1,28 @@
 import { ethers } from "ethers";
 import React, { useState } from "react";
 import "./App.css";
-import contractABI from "./contractABI.js";
+import { access } from "fs";
+const { abi } = require("./Staking.json");
+var signer;
+var account;
+if (typeof window.ethereum !== "undefined" || typeof window.web3 !== "undefined") {
+	// Web3 browser user detected. You can now use the provider.
+	const accounts = await window.ethereum.enable();
+	// const curProvider = window['ethereum'] || window.web3.currentProvider
 
-// const contractAddress = "0x98223247A13109f37C8118Ca669C18f41c89A8d8";
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner();
-// const contract = new ethers.Contract(contractAddress, contractABI, signer);
+	const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+	console.log("accounts: ", accounts);
+	console.log("provider: ", provider);
+	account = accounts[0];
+	signer = provider.getSigner();
+}
+const contractAddress = "0x98223247A13109f37C8118Ca669C18f41c89A8d8";
+
+console.log(abi);
+const contract = new ethers.Contract(contractAddress, abi, signer);
+console.log(contract.address);
+
 let contractOwnerAddress = 0;
 let signerAddress = 0;
 let signerStaked = 0;
@@ -14,80 +30,86 @@ let signerReward = 0;
 let ownerAuth = false;
 
 async function getSignerAddress() {
-  signerAddress = await signer.getAddress();
+	signerAddress = signer._address;
 }
 
 async function getContractOwnerAddress() {
-  contractOwnerAddress = 0;
-  // contractOwnerAddress = await contract.m_owner();
+	contractOwnerAddress = 0;
+	//contractOwnerAddress = await contract.m_owner();
 }
 
 async function getSignerMoney() {
-  // time in seconds
-  signerStaked = 20;
-  signerReward = 5;
+	// time in seconds
+	signerStaked = 20;
+	signerReward = 5;
 }
 
 async function stake() {
-  return 0;
+	const tx = await contract.methods.stake(1111).send({
+		from: account,
+	});
+	tx.wait();
+	console.log(tx.receipt);
 }
 
 async function withdrawStake() {
-  return 0;
+	const tx = await contract.withdrawStake(1111);
+	tx.wait();
+	console.log(tx.receipt);
 }
 
 async function claimRewards() {
-  return 0;
+	return 0;
 }
 
 function StakingContract(props) {
-  return (
-    <div className="stake-container">
-      <div className="stake-text">
-        <h3>Staked: {signerStaked}</h3>
-        <h3>Rewards: {signerReward}</h3>
-      </div>
-      <div className="stake-buttons">
-        <button className="btn primary-btn" onClick={() => stake()}>
-          Stake
-        </button>
-        <button className="btn secondary-btn" onClick={() => withdrawStake()}>
-          Withdraw stake
-        </button>
-        <button className="btn secondary-btn" onClick={() => claimRewards()}>
-          Claim rewards
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="stake-container">
+			<div className="stake-text">
+				<h3>Staked: {signerStaked}</h3>
+				<h3>Rewards: {signerReward}</h3>
+			</div>
+			<div className="stake-buttons">
+				<button className="btn primary-btn" onClick={() => stake()}>
+					Stake
+				</button>
+				<button className="btn secondary-btn" onClick={() => withdrawStake()}>
+					Withdraw stake
+				</button>
+				<button className="btn secondary-btn" onClick={() => claimRewards()}>
+					Claim rewards
+				</button>
+			</div>
+		</div>
+	);
 }
 
 class App extends React.Component {
-  componentDidMount() {
-    // before we render we do this
-    this.setState({ loading: true });
-    getContractOwnerAddress().then(() => {
-      getSignerAddress().then(() => {
-        if (signerAddress == contractOwnerAddress) {
-          ownerAuth = true;
-        }
-      });
-    });
-    getSignerMoney().then(() => {
-      // while loading is true we won't render
-      this.setState({ loading: false });
-    });
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <h1 className="head-text">Stake!</h1>
-          <StakingContract />
-        </div>
-      </div>
-    );
-  }
+	componentDidMount() {
+		// before we render we do this
+		this.setState({ loading: true });
+		getContractOwnerAddress().then(() => {
+			getSignerAddress().then(() => {
+				if (signerAddress == contractOwnerAddress) {
+					ownerAuth = true;
+				}
+			});
+		});
+		getSignerMoney().then(() => {
+			// while loading is true we won't render
+			this.setState({ loading: false });
+		});
+	}
+	render() {
+		return (
+			<div className="App">
+				<div className="container">
+					<h1 className="head-text">Stake!</h1>
+					<StakingContract />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
